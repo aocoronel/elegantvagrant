fe7ecc9b22e610c08fbece9486916adb08fd8f26
Augusto Coronel <aocoronel@proton.me>
2026-08-23 17:11:52 -0300

    emacs: ... feat: improve eglot colors chore: align and deduplicate faces Swapped vterm faces with ansi-term fix: elegantvagrant-orgmode works now

--- emacs/elegantvagrant-theme.el
+++ emacs/elegantvagrant-theme.el
@@ -7,39 +7,25 @@
 
 (defun elegantvagrant-orgmode ()
   (interactive)
-  (let ((default-font (font-spec :family elegantvagrant-font :size 14)))
+  (let ((base-size 14))
     (dolist (face
              '((org-level-1 1.3 "#F38BA8" ultra-bold)
                (org-level-2 1.3 "#F3B387" extra-bold)
                (org-level-3 1.3 "#A6E3A1" bold)
                (org-level-4 1.3 "#90E2D5" semi-bold)
                (org-level-5 1.3 "#A0BEFE" normal)
-               (org-level-6 1.3 "#cba6cb" normal)
-               (org-level-7 1.3 "#46d9ff" normal)
-               (org-level-8 1.3 "#ff6c6b" normal)))
-      (set-face-attribute (nth 0 face) 'unspecified
-                          :font (font-spec :family elegantvagrant-variable-pitch-font
-                                         :size (round (* 14 (nth 1 face))))
-                          :weight (nth 3 face)
-                          :foreground (nth 2 face)))
-    (set-face-attribute 'org-table nil
-                        :font default-font
-                        :weight 'normal
-                        :height 1.0
-                        :foreground "#bfafdf")))
+               (org-level-6 1.3 "#CBA6CB" normal)))
+      (set-face-attribute (nth 0 face) nil :foreground (nth 2 face)))
+
+    (set-face-attribute 'org-table nil :foreground "#BFAFDF")))
 
 (let* ((bg         "#000000")
-       (bg-alt     "#121212")
        (base0      "#141414")
        (base1      "#161616")
        (base2      "#202020")
        (base3      "#585b70")
        (base4      "#6c7086")
        (base5      "#7f849c")
-       (base6      "#9399b2")
-       (base7      "#a6adc8")
-       (base8      "#bac2de")
-       (fg-alt     "#BAC9E4")
        (fg         "#e4e4ef")
        (red        "#f38ba8")
        (orange     "#fab387")
@@ -74,28 +60,25 @@
    'elegantvagrant
 
    ;; Basic UI
-   `(default              ((t (:foreground ,fg :background ,bg))))
-   `(cursor               ((t (:background ,accent))))
-   `(fringe               ((t (:background ,base1))))
-   `(region               ((t (:background ,base2))))
-   `(highlight            ((t (:foreground unspecified :background ,violet))))
-   `(vertical-border      ((t (:foreground ,base2))))
-   `(minibuffer-prompt    ((t (:foreground ,blue :weight bold))))
+   `(default				((t (:foreground ,fg :background ,bg))))
+   `(cursor					((t (:background ,accent))))
+   `(fringe					((t (:background ,base1))))
+   `(region					((t (:background ,base2))))
+   `(highlight				((t (:foreground unspecified :background ,violet))))
+   `(vertical-border		((t (:foreground ,base2))))
+   `(minibuffer-prompt		((t (:foreground ,blue :weight bold))))
+   `(match					((t (:foreground ,bg :background ,yellow))))
+   `(link					((t (:foreground ,yellow :underline t))))
+   `(link-visited			((t (:foreground ,cyan :underline t))))
+   `(border					((t (:background ,bg :foreground ,base3))))
+   `(shadow					((t (:foreground ,base3))))
+   `(trailing-whitespace	((t (:foreground ,base3 :background ,red))))
 
-   `(match ((t (:foreground ,bg :background ,yellow))))
-   `(link ((t (:foreground ,yellow :underline t))))
-   `(link-visited ((t (:foreground ,cyan :underline t))))
-
-   `(border ((t (:background ,bg :foreground ,base3))))
-   `(shadow ((t (:foreground ,base3))))
-   `(trailing-whitespace ((t (:foreground ,base3 :background ,red))))
-
-    ;; Markdown - Different colors per heading level
+   ;; Markdown
    `(markdown-markup-face             ((t (:foreground ,base5))))
    `(markdown-header-face             ((t (:inherit bold :foreground ,red))))
    `(markdown-code-face               ((t (:background ,nil))))
    `(markdown-inline-code-face        ((t (:background ,nil))))
-
    `(markdown-header-face             ((t (:inherit bold :foreground ,red))))
    `(markdown-header-face-1           ((t (:inherit bold :foreground ,blue))))
    `(markdown-header-face-2           ((t (:inherit bold :foreground ,orange))))
@@ -104,27 +87,14 @@
    `(markdown-header-face-5           ((t (:inherit bold :foreground ,yellow))))
    `(markdown-header-face-6           ((t (:inherit bold :foreground ,violet))))
 
-   ;; Org-mode
-   `(org-block ((t (:background ,base1))))
-   `(org-block-begin-line ((t (:background ,base1 :foreground ,violet))))
-   `(org-hide ((t (:foreground ,base4))))
-   `(org-link ((t (:foreground ,orange :underline t :weight bold))))
-
-   ;; Org Mode
-   `(org-agenda-structure ((t (:foreground ,base5))))
-   `(org-column ((t (:background ,base2))))
-   `(org-done ((t (:foreground ,green))))
-   `(org-todo ((t (:foreground ,maroon))))
-   `(org-upcoming-deadline ((t (:foreground ,yellow))))
-
    ;; Diff
-   `(diff-removed ((t (:foreground ,red))))
-   `(diff-added ((t (:foreground ,green))))
+   `(diff-removed	((t (:foreground ,red))))
+   `(diff-added		((t (:foreground ,green))))
 
    ;; Compilation
-   `(compilation-info ((t (:foreground ,green))))
-   `(compilation-warning ((t (:foreground ,peach :bold t))))
-   `(compilation-error ((t (:foreground ,red))))
+   `(compilation-info			((t (:foreground ,green))))
+   `(compilation-warning		((t (:foreground ,peach :bold t))))
+   `(compilation-error			((t (:foreground ,red))))
    `(compilation-mode-line-fail ((t (:foreground ,red :weight bold))))
    `(compilation-mode-line-exit ((t (:foreground ,green :weight bold))))
 
@@ -132,24 +102,19 @@
    `(dired-directory ((t (:foreground ,accent :weight bold))))
 
    ;; Magit
-   `(magit-branch ((t (:foreground ,fg))))
+   `(magit-branch			((t (:foreground ,fg))))
    `(magit-diff-hunk-header ((t (:background ,base1))))
-   `(magit-item-highlight ((t (:background ,base2))))
-   `(magit-tag ((t (:foreground ,yellow :background ,base0))))
+   `(magit-item-highlight	((t (:background ,base2))))
+   `(magit-tag				((t (:foreground ,yellow :background ,base0))))
 
    ;; Show Paren
-   `(show-paren-match-face ((t (:background ,base3))))
-   `(show-paren-mismatch-face ((t (:background ,maroon))))
+   `(show-paren-match-face		((t (:background ,base3))))
+   `(show-paren-mismatch-face	((t (:background ,maroon))))
 
    ;; tab-bar
-   `(tab-bar ((t (:background ,base2 :foreground ,base3))))
-   `(tab-bar-tab ((t (:background ,'unspecified :foreground ,yellow :weight bold))))
-   `(tab-bar-tab-inactive ((t (:background ,'unspecified))))
-
-   ;; Company
-   `(company-tooltip ((t (:foreground ,fg :background ,base2))))
-   `(company-tooltip-selection ((t (:foreground ,fg :background ,base1))))
-   `(company-tooltip-annotation ((t (:foreground ,peach))))
+   `(tab-bar				((t (:background ,base2 :foreground ,base3))))
+   `(tab-bar-tab			((t (:background ,'unspecified :foreground ,yellow :weight bold))))
+   `(tab-bar-tab-inactive	((t (:background ,'unspecified))))
 
    ;; Orderless
    `(orderless-match-face-0 ((t (:foreground ,yellow))))
@@ -177,17 +142,17 @@
                          (t (:foreground ,yellow :weight bold :underline t))))
 
    ;; Line numbers
-   `(line-number                      ((t (:foreground ,base2))))
-   `(line-number-current-line         ((t (:foreground "#BAC9E4"))))
-   `(hl-line                            ((t (:background ,base1))))
-   `(current-line-highlight             ((t (:background ,base1))))
+   `(line-number                ((t (:foreground ,base2))))
+   `(line-number-current-line   ((t (:foreground "#BAC9E4"))))
+   `(hl-line                    ((t (:background ,base1))))
+   `(current-line-highlight     ((t (:background ,base1))))
 
    ;; Mode-line
-   `(mode-line                        ((t (:background ,base0 :foreground unspecified
+   `(mode-line          ((t (:background ,base0 :foreground unspecified
                                                        :box (:color ,base1)))))
-   `(mode-line-inactive               ((t (:background ,base1 :foreground ,base5
+   `(mode-line-inactive ((t (:background ,base1 :foreground ,base5
                                                        :box (:color ,base2)))))
-   `(mode-line-emphasis               ((t (:foreground ,violet))))
+   `(mode-line-emphasis ((t (:foreground ,violet))))
 
    ;; Font lock
    `(font-lock-comment-face           ((t (:background ,bg :foreground "#eba0ac"))))
@@ -201,46 +166,75 @@
    `(font-lock-builtin-face           ((t (:foreground ,mauve))))
    `(font-lock-warning-face           ((t (:foreground ,yellow))))
 
-   ;; Secondary selection
-   `(secondary-selection              ((t (:background ,base0))))
+   ;; Eglot
+   `(eglot-semantic-defaultLibrary  ((t (:foreground ,peach))))
+   `(eglot-semantic-operator        ((t (:foreground ,white))))
+   `(eglot-semantic-definition      ((t (:foreground ,white))))
+   `(eglot-semantic-declaration     ((t (:foreground ,white))))
+   `(eglot-semantic-enum            ((t (:foreground ,teal))))
+   `(eglot-semantic-enumMember      ((t (:foreground ,teal))))
+   `(eglot-semantic-readonly        ((t (:foreground ,teal))))
+   `(eglot-semantic-number          ((t (:foreground ,peach))))
+   ;; `(eglot-semantic-namespace		((t (:foreground ,white))))
+   ;; `(eglot-semantic-type				((t (:foreground ,white))))
+   ;; `(eglot-semantic-class			((t (:foreground ,white))))
+   ;; `(eglot-semantic-interface		((t (:foreground ,white))))
+   ;; `(eglot-semantic-struct			((t (:foreground ,white))))
+   ;; `(eglot-semantic-typeParameter	((t (:foreground ,white))))
+   ;; `(eglot-semantic-parameter		((t (:foreground ,white))))
+   ;; `(eglot-semantic-variable			((t (:foreground ,white))))
+   ;; `(eglot-semantic-property			((t (:foreground ,white))))
+   ;; `(eglot-semantic-event			((t (:foreground ,white))))
+   ;; `(eglot-semantic-function			((t (:foreground ,white))))
+   ;; `(eglot-semantic-method			((t (:foreground ,white))))
+   ;; `(eglot-semantic-macro			((t (:foreground ,white))))
+   ;; `(eglot-semantic-keyword			((t (:foreground ,white))))
+   ;; `(eglot-semantic-modifier			((t (:foreground ,white))))
+   ;; `(eglot-semantic-comment			((t (:foreground ,white))))
+   ;; `(eglot-semantic-string			((t (:foreground ,white))))
+   ;; `(eglot-semantic-regexp			((t (:foreground ,white))))
+   ;; `(eglot-semantic-decorator		((t (:foreground ,white))))
+   ;; `(eglot-semantic-static			((t (:foreground ,white))))
+   ;; `(eglot-semantic-deprecated		((t (:foreground ,white))))
+   ;; `(eglot-semantic-abstract			((t (:foreground ,white))))
+   ;; `(eglot-semantic-async			((t (:foreground ,white))))
+   ;; `(eglot-semantic-modification		((t (:foreground ,white))))
+   ;; `(eglot-semantic-documentation	((t (:foreground ,white))))
 
-   ;; Tooltip
-   `(tooltip                          ((t (:foreground ,fg :background ,base0))))
+   `(secondary-selection		((t (:background ,base0))))
+   `(tooltip					((t (:foreground ,fg :background ,base0))))
+   `(css-proprietary-property   ((t (:foreground ,orange))))
+   `(css-property               ((t (:foreground ,green))))
+   `(css-selector               ((t (:foreground ,blue))))
 
-   ;; CSS
-   `(css-proprietary-property         ((t (:foreground ,orange))))
-   `(css-property                     ((t (:foreground ,green))))
-   `(css-selector                     ((t (:foreground ,blue))))
+   ;; Org-mode
+   `(org-agenda-structure	((t (:foreground ,base5))))
+   `(org-block				((t (:background ,base1))))
+   `(org-block-begin-line   ((t (:background ,base1 :foreground ,comments))))
+   `(org-column				((t (:background ,base2))))
+   `(org-done				((t (:foreground ,green))))
+   `(org-hide               ((t (:foreground ,bg))))
+   `(org-link               ((t (:foreground ,orange :underline t :weight bold))))
+   `(org-todo				((t (:foreground ,maroon))))
+   `(org-upcoming-deadline	((t (:foreground ,yellow))))
 
-   ;; Doom modeline (fallback styling)
-   `(doom-modeline-bar                ((t (:background ,violet))))
-
-   ;; Elscreen
-   `(elscreen-tab-other-screen-face   ((t (:background "#353a42" :foreground "#1e2022"))))
-
-   ;; Org
-   `(org-block                        ((t (:background ,base1))))
-   `(org-block-begin-line             ((t (:background ,base1 :foreground ,comments))))
-   `(org-hide                         ((t (:foreground ,bg))))
-   `(org-link                         ((t (:foreground ,orange :underline t :weight bold))))
-
-   ;; VTerm
-   `(vterm-color-black          ((t (:foreground ,"#1c1f24"))))
-   `(vterm-color-red            ((t (:foreground ,"#fc2025"))))
-   `(vterm-color-green          ((t (:foreground ,"#02f789"))))
-   `(vterm-color-yellow         ((t (:foreground ,"#f9fc5f"))))
-   `(vterm-color-blue           ((t (:foreground ,"#5ffcfc"))))
-   `(vterm-color-magenta        ((t (:foreground ,"#f067fc"))))
-   `(vterm-color-cyan           ((t (:foreground ,"#00b29e"))))
-   `(vterm-color-white          ((t (:foreground ,"#e9e2cb"))))
-   `(vterm-color-bright-black   ((t (:foreground ,"#202020"))))
-   `(vterm-color-bright-red     ((t (:foreground ,"#f4153b"))))
-   `(vterm-color-bright-green   ((t (:foreground ,"#50ee84"))))
-   `(vterm-color-bright-yellow  ((t (:foreground ,"#fcab20"))))
-   `(vterm-color-bright-blue    ((t (:foreground ,"#20dbfc"))))
-   `(vterm-color-bright-magenta ((t (:foreground ,"#c61b6e"))))
-   `(vterm-color-bright-cyan    ((t (:foreground ,"#00b27f"))))
-   `(vterm-color-bright-white   ((t (:foreground ,"#fcf4dc"))))
+   ;; Ansi Terminal
+   `(ansi-color-black          ((t (:foreground ,"#1c1f24"))))
+   `(ansi-color-red            ((t (:foreground ,"#fc2025"))))
+   `(ansi-color-green          ((t (:foreground ,"#02f789"))))
+   `(ansi-color-yellow         ((t (:foreground ,"#f9fc5f"))))
+   `(ansi-color-blue           ((t (:foreground ,"#5ffcfc"))))
+   `(ansi-color-magenta        ((t (:foreground ,"#f067fc"))))
+   `(ansi-color-cyan           ((t (:foreground ,"#00b29e"))))
+   `(ansi-color-white          ((t (:foreground ,"#e9e2cb"))))
+   `(ansi-color-bright-black   ((t (:foreground ,"#202020"))))
+   `(ansi-color-bright-red     ((t (:foreground ,"#f4153b"))))
+   `(ansi-color-bright-green   ((t (:foreground ,"#50ee84"))))
+   `(ansi-color-bright-yellow  ((t (:foreground ,"#fcab20"))))
+   `(ansi-color-bright-blue    ((t (:foreground ,"#20dbfc"))))
+   `(ansi-color-bright-magenta ((t (:foreground ,"#c61b6e"))))
+   `(ansi-color-bright-cyan    ((t (:foreground ,"#00b27f"))))
+   `(ansi-color-bright-white   ((t (:foreground ,"#fcf4dc"))))
 
    `(whitespace-space       ((t (:foreground ,"#202020" :background , "#000000"))))
    `(whitespace-indentation ((t (:foreground ,"#202020" :background , "#202020"))))
